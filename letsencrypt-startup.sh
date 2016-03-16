87adbba73ad13315addda49611537802541a6cbb
Petr <webmaster@netfinity.cz>
2016-03-16 13:09:37 +0100

    update

--- letsencrypt-startup.sh
+++ letsencrypt-startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f /etc/secrets/letsencrypt/signed.crt ]; then
-	busybox httpd -f -p 80 -u www-user:www-user -h /var/app-cert
+	busybox httpd -f -p 80 -u www-user:www-user -h /var/app-cert &
 	HTTP_PID=$!
 	/etc/cron.monthly/letsencrypt.sh
 	kill $HTTP_PID
